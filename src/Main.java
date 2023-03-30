d55e7aa7ad8d388b20dc7b23aada80f42025b191
Olga Malkova <Vodeviiil@mail.ru>
2023-03-30 17:12:44 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println(
+                MoneyCalculator.calculateCassetteCount(1999)
+        );
+        System.out.println(
+                MoneyCalculator.calculateCassetteCount(2000)
+        );
+        System.out.println(
+                MoneyCalculator.calculateCassetteCount(2001)
+        );
+    }
+}
